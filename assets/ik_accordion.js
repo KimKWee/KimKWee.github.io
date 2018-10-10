d1d3a74f414ecce4a075630cb7c04f4062ed1421
KimKWee <43556683+KimKWee@users.noreply.github.com>
2018-10-09 21:07:57 -0400

    Update ik_accordion.js

--- assets/ik_accordion.js
+++ assets/ik_accordion.js
@@ -41,6 +41,7 @@
 			var $me, $btn;
 			
 			$me = $(el);
+			$me.attr({'role': 'heading'}); // set heading role for each accordion header
 			$btn = $('<div/>').attr({
 				'id': id + '_btn_' + i,
 				'role': 'button',
@@ -50,6 +51,7 @@
 			})
 			.addClass('button')
 			.html($me.html())
+			.on('keydown', {'plugin': plugin}, plugin.onKeyDown) // enable keyboard navigation
 			.on('click', {'plugin': plugin}, plugin.togglePanel);
         
 			$me.empty().append($btn); // wrap content of each header in an element with role button
@@ -58,11 +60,59 @@
 		this.panels = $elem.children('dd').each(function(i, el) {
 			var $me = $(this), id = $elem.attr('id') + '_panel_' + i;
 			$me.attr({
-				'id': id
+				'id': id,
+				'role': 'region', // add role region to each panel
+				'aria-hidden': true, // mark all panels as hidden
+				'tabindex': 0 // add panels into the tab order
 			});
 		}).hide();
 		
 	};
+	
+	/**
+     * Handles kedown event on header button.
+     *
+     * @param {Object} event - Keyboard event.
+     * @param {object} event.data - Event data.
+     * @param {object} event.data.plugin - Reference to plugin.
+     */
+    Plugin.prototype.onKeyDown = function (event) {
+       
+        var $me, $header, plugin, $elem, $current, ind;
+       
+        $me = $(event.target);
+        $header = $me.parent('dt');
+        plugin = event.data.plugin;
+        $elem = $(plugin.element);
+       
+        switch (event.keyCode) {
+           
+            // toggle panel by pressing enter key, or spacebar
+            case ik_utils.keys.enter:
+            case ik_utils.keys.space:
+                event.preventDefault();
+                event.stopPropagation();
+                plugin.togglePanel(event);
+                break;
+           
+            // use up arrow to jump to the previous header
+            case ik_utils.keys.up:
+                ind = plugin.headers.index($header);
+                if (ind > 0) {
+                    plugin.headers.eq(--ind).find('.button').focus();
+                }
+                console.log(ind);
+                break;
+           
+            // use down arrow to jump to the next header
+            case ik_utils.keys.down:
+                ind = plugin.headers.index($header);
+                if (ind < plugin.headers.length - 1) {
+                    plugin.headers.eq(++ind).find('.button').focus();
+                }
+                break;
+        }
+    };
 	
 	/** 
 	 * Toggles accordion panel.
